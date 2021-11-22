2eaf33651493557715b1fb2d2d42bab56a252dd8
Gabriel Koch Fodi <gabrielkochfodi@gmail.com>
2021-11-21 22:18:40 -0300

    first version

---
+++ src/pages/app/polls/index.tsx
@@ -0,0 +1,10 @@
+export default function Polls() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <h1 className="mx-2">
+        Coming soon... Here we gonna open a voting system to decide what our DAO
+        will do to grow.
+      </h1>
+    </div>
+  );
+}
